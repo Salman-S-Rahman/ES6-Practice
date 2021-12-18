1f9174b2117bb5a91dd73d4de88c242d53b7e7b5
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-18 23:10:30 +0600

    added

---
+++ arrayDestructer.js
@@ -0,0 +1,9 @@
+
+var brotherName = ["MohibJan", "Roohullah", "Zabiullah", "AsmatUllah", "Asghar jan"];
+let [brother1, brother2, brother3, brother4, brother5] = brotherName;
+
+console.log(brother1);
+console.log(brother2);
+console.log(brother3);
+console.log(brother4);
+console.log(brother5);
