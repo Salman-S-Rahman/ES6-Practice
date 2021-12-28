650d67096dc43383e8adfe13ef4e25422229b1d1
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-28 23:01:56 +0600

    added file

---
+++ forIn.js
@@ -0,0 +1,13 @@
+
+var studentInfo = {
+    name: "Taskin Asghar",
+    id: 44,
+    studies: "EEE",
+    pashion: "MERN"
+}
+
+for (let info in studentInfo) {
+    console.log(`${info} : ${studentInfo[info]}`);
+}
+
+// ============= for of simply use for in array ==========
