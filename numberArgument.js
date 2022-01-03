e0e7622a7e3a1673677675d4df45ebd84f525265
Salman-S-Rahman <salmanmern@gmail.com>
2022-01-03 23:04:18 +0600

    added file

---
+++ numberArgument.js
@@ -0,0 +1,11 @@
+function totalNumber(number1, number2) {
+    let total = 0;
+    console.log(arguments);
+    for (number of arguments) {
+        total = total + number
+    }
+    return total;
+}
+const sum = totalNumber(10, 20, 30, 40, 50);
+
+console.log("Total = " + sum);
